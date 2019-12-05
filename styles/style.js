9299b311779e6f995e0de474c81f1b4dd154af73
Celestine <celesclaks@yahoo.com>
2019-12-05 10:35:33 -1200

    Solution

--- styles/style.js
+++ styles/style.js
@@ -13,35 +13,3 @@ const navSlide = () => {
     });
 }
 navSlide();
-
-$('.card-dec').slick({
-    infinite: false,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    responsive: [
-        {
-            breakpoint: 1300,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 1050,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        }
-
-    ]
-});
